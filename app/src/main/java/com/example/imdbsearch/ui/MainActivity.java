f1f25eb9820035eccc2274a7dabb698b7ae97264
mahmoud-abdallah863 <mabdallah863@gmail.com>
2020-03-29 03:52:59 +0300

    Added intro screen & animations

--- app/src/main/java/com/example/imdbsearch/ui/MainActivity.java
+++ app/src/main/java/com/example/imdbsearch/ui/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.imdbsearch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.assist.AssistStructure;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.imdbsearch.R;
 import com.example.imdbsearch.ui.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
 
         getSupportFragmentManager().beginTransaction()
--- app/src/main/java/com/example/imdbsearch/ui/fragments/MainFragment.java
+++ app/src/main/java/com/example/imdbsearch/ui/fragments/MainFragment.java
@@ -1,5 +1,8 @@
 package com.example.imdbsearch.ui.fragments;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,8 +13,11 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +29,25 @@ import butterknife.OnClick;
 
 public class MainFragment extends Fragment {
 
+    @BindView(R.id.cinemaImage)
+    public ImageView cinemaImage;
+
     @BindView(R.id.title)
     public TextView title_textView;
 
+    @BindView(R.id.internetError)
+    public TextView internetError;
+
     @BindView(R.id.movieName_editText)
     public EditText movieName_editText;
+
+    @BindView(R.id.search_btn)
+    public Button search_btn;
+
+    private Animation animation, cinemaAnimation;
+
+    private boolean connected = false;
+    private boolean firstTime = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,9 +61,72 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        if (firstTime) {
+            startCinemaAnimation();
+            setTitle();
+        } else {
+            connected = true;
+            setData();
+        }
+    }
+
+    private void checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        connected = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
+        setData();
+    }
+
+    private void setData() {
+        if (connected) {
+            cinemaImage.setVisibility(View.GONE);
+            internetError.setVisibility(View.GONE);
+            movieName_editText.setVisibility(View.VISIBLE);
+            search_btn.setVisibility(View.VISIBLE);
+            setTitle();
+            startAnimation();
+        } else {
+            internetError.setText("You are not connected to the internet...\nPlease connect and re-start the app");
+        }
+    }
+
+    private void setTitle() {
         String str = "Easy search ";
         String word = "<font color='#DBA506'>movies</font>";
         title_textView.setText(Html.fromHtml(str + word));
+    }
+
+    private void startCinemaAnimation() {
+        cinemaAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.cinema_image_animation);
+        cinemaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                checkConnection();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        cinemaImage.setAnimation(cinemaAnimation);
+    }
+
+    private void startAnimation() {
+        animation = AnimationUtils.loadAnimation(getContext(), R.anim.intro_animation);
+
+        movieName_editText.setAnimation(animation);
+        search_btn.setAnimation(animation);
     }
 
 
@@ -51,11 +134,12 @@ public class MainFragment extends Fragment {
     void searchClicked() {
         String movieName = movieName_editText.getText().toString().trim();
         if (movieName.length() != 0) {
-            movieName_editText.setText("");
             getActivity().getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new ResultFragment(movieName))
                     .addToBackStack("result")
                     .commit();
+            movieName_editText.setText("");
+            firstTime = false;
         } else {
             Toast.makeText(getContext(), "Please, enter a movie name", Toast.LENGTH_SHORT).show();
         }
